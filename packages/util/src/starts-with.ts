c45c1fe96c5c2cdc48934508aabb8079d0f5b0fb
刘放 <362512489@qq.com>
2020-06-23 11:32:36 +0800

    docs: remark

--- packages/util/src/starts-with.ts
+++ packages/util/src/starts-with.ts
@@ -1,6 +1,6 @@
 import isArray from './is-array';
 import isString from './is-string';
-
+//函数重载，不同入参类型返回结果类型也不同
 function startsWith(arr: string, e: string): boolean;
 function startsWith<T>(arr: T[], e: T): boolean;
 function startsWith<T>(arr: string | T[], e: string | T): boolean {
